8dc5703fe65e789195207517971b134454f9df7b
محمدجواد عباس زاده <m.abbaszadeh@apadanagroup.org>
2026-09-17 19:39:53 +0330

    Deploy snapshot: refresh vendor

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'c434cdc89c17f9484d651ddfe9d3d3ec6e7ca9cb',
+        'reference' => '4f1ca6b8f812ea326a01d4c8f162a2e10049cf61',
         'name' => 'laravel/laravel',
         'dev' => false,
     ),
@@ -373,7 +373,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'c434cdc89c17f9484d651ddfe9d3d3ec6e7ca9cb',
+            'reference' => '4f1ca6b8f812ea326a01d4c8f162a2e10049cf61',
             'dev_requirement' => false,
         ),
         'laravel/prompts' => array(
